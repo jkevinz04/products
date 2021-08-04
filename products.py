34a8bd17d198c873af38aabc866a425849545540
Yu-Chun Tsai <jkevinz04@gmail.com>
2021-08-04 21:56:54 +0800

    add read file function

--- products.py
+++ products.py
@@ -1,6 +1,18 @@
 #二維清單
 file = 'products.csv' #建議以 csv 檔案
 products = [] #大清單
+
+#讀取檔案
+with open(file, 'r') as f:
+    for line in f:
+        if '商品,價格' in line: #跳過存取標題
+            continue #跳過此回進入下一回
+        #name, price = line.strip().split(',')
+        #products.append([name, price])
+        products.append(line.strip().split(',')) #list comprehension
+print(products)
+
+#讓使用者輸入
 while True:
     name = input('請輸入商品名稱： ')
     if name == 'q':
@@ -15,12 +27,13 @@ while True:
     products.append([name, price]) #list comprehension
 print(products)
 
-#依序存取大清單中的小清單
-for p in products:
+#印出購買紀錄
+for p in products:#依序存取大清單中的小清單
     print (p[0], '的價格是', p[1]) #依序印出小清單的第 0 位置及第 1 位置
     
 #print(products[0][0]) #存取大清單第 0 位置中的小清單第 0 位置
 
+#將新增品項寫入檔案
 with open(file, 'w') as f: #encoding='utf-8' 如需解決亂碼問題則在'w'後最後加入
     f.write('商品,價格\n')
     for p in products:
